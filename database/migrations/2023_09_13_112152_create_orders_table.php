58090c6073f886b965e82e6f14d0765e48dd1dc1
mahsare-khalili <mahsare.khalili@gmail.com>
2023-09-13 09:15:45 -0400

    initial project

---
+++ database/migrations/2023_09_13_112152_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('code', 250)->unique();
+            $table->integer('amount');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+
+        \DB::update('ALTER TABLE orders AUTO_INCREMENT = 100000');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
